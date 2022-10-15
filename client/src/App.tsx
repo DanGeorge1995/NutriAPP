69ec7cb883181b20e459ee0277449208db7aa606
DanGeorge1995 <dangeorgeburghelea@yahoo.com>
2022-10-15 20:01:01 +0300

    added redux template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,24 @@
 import "./styles/global.scss";
+import type { RootState } from "./redux/store/store";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/slices/counterSlice";
 
 function App() {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <>
-      <h1>HELLO WORLD</h1>
+      <div>
+        <div>
+          <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+            Increment
+          </button>
+          <span>{count}</span>
+          <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+            Decrement
+          </button>
+        </div>
+      </div>
     </>
   );
 }
